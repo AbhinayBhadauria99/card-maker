cf6cce9fb0a9e0cd17f735fa1a30645176d87978
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-03-08 02:59:16 +0530

    added useState for fetching data entered by user

--- user/src/components/Create.jsx
+++ user/src/components/Create.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [age, setAge] = useState(0);
+
+    console.log(name, email, age);
+
     return (
         <div classNamename="container my-2">
             <h2 classNamename="text-center">Enter your Card</h2>
@@ -8,16 +15,16 @@ const Create = () => {
             <form>
                 <div className="mb-3">
                     <label className="form-label">Name</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" value={name} onChange={(e) => setName(e.target.value)} />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Age</label>
-                    <input type="number" className="form-control" />
+                    <input type="number" className="form-control" value={age} onChange={(e) => setAge(e.target.value)} />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
